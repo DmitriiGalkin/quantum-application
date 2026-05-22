c9638f6c1f975a97fc001f357d0f11a26f29f6b5
Дмитрий <4757037@gmail.com>
2026-05-22 22:52:35 +0300

    ускорение 4

--- application/src/main.tsx
+++ application/src/main.tsx
@@ -3,6 +3,7 @@ import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
+import 'normalize.css';
 
 const queryClient = new QueryClient();
 
