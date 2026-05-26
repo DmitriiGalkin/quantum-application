e356118edcd52b4ce8e8c6f6c3115ececcbf4321
Дмитрий <4757037@gmail.com>
2026-05-26 23:02:47 +0300

    mui opt

--- application/src/ProjectCard.tsx
+++ application/src/ProjectCard.tsx
@@ -7,7 +7,7 @@ import {
   Typography,
 } from '@mui/material';
 import type { Project } from './types';
-import { AutoAwesome } from '@mui/icons-material';
+import AutoAwesome from '@mui/icons-material/AutoAwesome';
 
 type ProjectCardProps = {
   project: Project;
