8abe316387e8bb0fe1fbbdd633de3a1c863071b7
Дмитрий <4757037@gmail.com>
2026-06-04 21:07:37 +0300

    server 9

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -1,22 +1,23 @@
-const path = require('path');
+// Получаем абсолютный путь к директории с этим файлом
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default {
-  apps: [
-    {
-      name: 'backend',
-      script: path.join(__dirname, 'backend/src/index.ts'),
-      out_file: '/dev/stdout',
-      error_file: '/dev/stderr',
-      merge_logs: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    },
-    {
-      name: 'application',
-      script: path.join(__dirname, 'application/server.js'),
-      out_file: '/dev/stdout',
-      error_file: '/dev/stderr',
-      merge_logs: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    },
-  ],
-};
+export const apps = [
+  {
+    name: 'backend',
+    script: path.join(__dirname, 'backend/src/index.ts'),
+    out_file: '/dev/stdout',
+    error_file: '/dev/stderr',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  },
+  {
+    name: 'application',
+    script: path.join(__dirname, 'application/server.js'),
+    out_file: '/dev/stdout',
+    error_file: '/dev/stderr',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  },
+];
