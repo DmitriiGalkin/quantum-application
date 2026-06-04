c88b679d316b3cfd5ccee31656a9ec120252f468
Дмитрий <4757037@gmail.com>
2026-06-04 23:32:30 +0300

    server 15

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -6,12 +6,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export const apps = [
   {
     name: 'backend',
-    script: 'sh',
-    args: '-lc "cd ./backend && npm run build && node dist/index.ts"',
+    script: path.join(__dirname, 'backend/src/index.ts'),
     env: {
       NODE_ENV: 'node',
     },
-    cwd: '/workspace',
     out_file: '/dev/stdout',
     error_file: '/dev/stderr',
     merge_logs: true,
@@ -20,12 +18,30 @@ export const apps = [
   },
   {
     name: 'application',
-    script: 'application/server.ts',
+    script: path.join(__dirname, 'application/server.ts'),
     out_file: '/dev/stdout',
     error_file: '/dev/stderr',
     merge_logs: true,
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
     interpreter: 'node',
   },
+  // {
+  //   name: 'backend',
+  //   script: 'sh',
+  //   args: '-lc "cd ./backend && npm run build && node dist/index.ts"',
+  //   cwd: '/workspace',
+  //   out_file: '/dev/stdout',
+  //   error_file: '/dev/stderr',
+  //   merge_logs: true,
+  //   log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  // },
+  // {
+  //   name: 'application',
+  //   script: 'application/server.js',
+  //   out_file: '/dev/stdout',
+  //   error_file: '/dev/stderr',
+  //   merge_logs: true,
+  //   log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  // },
 ];
 
