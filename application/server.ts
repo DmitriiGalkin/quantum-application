686eb1e7052e42b56677f0eff17ef290dbf3dac5
Дмитрий <4757037@gmail.com>
2026-06-06 01:31:39 +0300

    ssr 9

--- application/server.ts
+++ application/server.ts
@@ -31,10 +31,10 @@ async function server() {
 
     app.use(vite.middlewares);
   } else {
-    console.log(path.resolve(__dirname, 'dist'), "path.resolve(__dirname, 'dist')");
+    console.log(path.resolve(__dirname, '/'), "path.resolve(__dirname, 'dist')");
 
     app.use(
-      express.static(path.resolve(__dirname, 'dist'), {
+      express.static(path.resolve(__dirname, '/'), {
         index: false,
       }),
     );
