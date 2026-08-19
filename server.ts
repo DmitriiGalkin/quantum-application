b33d1e0e004edf2f2187c5de35ee9ccef26b955c
Дмитрий <4757037@gmail.com>
2026-08-19 23:34:36 +0300

    На прод3

--- server.ts
+++ server.ts
@@ -6,9 +6,23 @@ import express from 'express';
 import { createServer } from 'vite';
 import { fileURLToPath } from 'url';
 import compression from 'compression';
-import { PageMeta } from '@shared/types';
 
 dotenv.config();
+
+export interface PageMeta {
+  title: string;
+  description: string;
+  // Заголовок страницы при шаринге (до 60 символов)
+  ogTitle: string;
+  // Краткое описание под заголовком (1–2 строки текста)
+  ogDescription: string;
+  // Картинка превью (рекомендации: JPG / PNG, 1200×630 px, абсолютный URL)
+  ogImage: string;
+  // Тип контента
+  ogType: string;
+  // Название сайта/бренда (Показывается мелким текстом. Не всегда отображается во всех платформах)
+  ogSiteName?: string;
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
